f6b6025b083097fdc1be12756606d9aad56d3914
Héricles Emanuel <hericles.me@gmail.com>
2020-01-21 15:34:13 -0300

    Update menu

---
+++ pyflash/config.py
@@ -0,0 +1,10 @@
+import os
+import configparser
+
+config = configparser.ConfigParser()
+
+config_path = os.path.dirname(os.path.abspath(__file__)) + '/../setup.cfg'
+
+config.read(config_path)
+
+games_dir = config.get('general', 'games_folder')
---
+++ pyflash/locations.py
@@ -0,0 +1,2 @@
+class LOCATION(obj):
+  GAMES = ""
